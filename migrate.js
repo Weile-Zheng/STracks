48cfaa8f1daf8bd8b5891f4f6d353130e8e4b7e5
Weile <wzheng13@u.rochester.edu>
2023-10-10 22:20:26 -0400

    Modularized Code

--- migrate.js
+++ migrate.js
@@ -1,10 +1,9 @@
 const { playlist_track_all } = require('./NeteaseCloudMusicApi')
-
 const fs = require('fs');
 
 let name
 
-async function test() {
+async function fetchPlaylistTracks() {
   try {
     const result = await playlist_track_all({
       id: '2065854146'
@@ -14,14 +13,24 @@ async function test() {
     song.forEach(obj => {
       songDict[obj.name] = obj.ar[0].name
     })
+    return songDict
 
-    fs.writeFile("./songs.txt", Object.entries(songDict).map(([key, value]) => `${key}: ${value}`).join('\n'), err => {
-      if (err) throw err
-      console.log("Output saved to file")
-    })
   } catch (error) {
     console.log(error)
   }
 }
 
-test()
+function writeDictToFile(playlist) {
+  fs.writeFile("./songs.txt", Object.entries(playlist)
+    .map(([key, value]) => `${key}: ${value}`).join('\n'), err => {
+      if (err) throw err
+      console.log("Output saved to file")
+    })
+}
+
+async function main() {
+  const dict = await fetchPlaylistTracks()
+  writeDictToFile(dict)
+}
+
+main()
